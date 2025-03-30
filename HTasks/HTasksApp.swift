b0e54797b7570ae73cea3d4835cb9f062ec5a30f
TheAppleUser11 <candy606755@duck.com>
2025-03-30 11:39:40 +0200

    Initial commit

---
+++ HTasks/HTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTasksApp.swift
+//  HTasks
+//
+//  Created by Apple on 2025. 03. 29..
+//
+
+import SwiftUI
+
+@main
+struct HTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HTasksTests/HTasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  HTasksTests.swift
+//  HTasksTests
+//
+//  Created by Apple on 2025. 03. 29..
+//
+
+import Testing
+@testable import HTasks
+
+struct HTasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
