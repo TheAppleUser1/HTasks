dd8127fd18787f0ca259605cc720c633215cd5a2
TheAppleUser11 <candy606755@duck.com>
2025-04-01 21:03:34 +0200

    medve

---
+++ HTasks/Views/CategoryPickerView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+import CoreData
+
+struct CategoryPickerView: View {
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject private var coreDataManager: CoreDataManager
+    @Binding var selectedCategory: CategoryEntity?
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Button(action: {
+                    selectedCategory = nil
+                    dismiss()
+                }) {
+                    HStack {
+                        Text("None")
+                        Spacer()
+                        if selectedCategory == nil {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
+                
+                ForEach(coreDataManager.fetchCategories()) { category in
+                    Button(action: {
+                        selectedCategory = category
+                        dismiss()
+                    }) {
+                        HStack {
+                            Text(category.name ?? "")
+                            Spacer()
+                            if selectedCategory?.id == category.id {
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Select Category")
+            .navigationBarItems(trailing: Button("Done") { dismiss() })
+        }
+    }
+} 
