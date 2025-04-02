0c81e0029f6ef542fe58ffccf73455538cef211e
TheAppleUser11 <candy606755@duck.com>
2025-04-02 16:34:02 +0200

    medve

--- HTasks/HTasksApp.swift
+++ HTasks/HTasksApp.swift
@@ -13,6 +13,13 @@ struct HTasksApp: App {
     @StateObject private var coreDataManager = CoreDataManager.shared
     @StateObject private var notificationManager = NotificationManager.shared
     
+    init() {
+        ValueTransformer.setValueTransformer(
+            UIColorTransformer(),
+            forName: NSValueTransformerName("UIColorTransformer")
+        )
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
